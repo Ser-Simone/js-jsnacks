9f6e32451bdca3fc117646106e527078c1a8b37c
Simone <simoneserpi@hotmail.it>
2022-03-25 13:15:56 +0100

    Primo_es

---
+++ main.js
@@ -0,0 +1,13 @@
+/*L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.*/
+
+let numero1 = prompt ("Inserisci il primo numero");
+let numero2 = prompt ("Inserisci il secondo numero");
+
+if(numero1>numero2)
+{
+    console.log(numero1);
+}else if(numero1<numero2)
+{
+    console.log(numero2);
+}
