0f3b565b0168abeea93e0e22fd2f7d61c116e708
Simone <simoneserpi@hotmail.it>
2022-03-25 14:03:56 +0100

    JS3_Finito

--- JSnack2/main.js
+++ JSnack2/main.js
@@ -2,13 +2,13 @@
 Il software stampa prima la parola più corta, poi la parola più lunga.*/
 
 
-let parola1 = prompt ("Inserisci il primo numero");
-let parola2 = prompt ("Inserisci il secondo numero");
+let parola1 = prompt ("Inserisci la prima parola");
+let parola2 = prompt ("Inserisci la seconda parola");
 
-if(parola1>parola2)
+if(parola1.length>parola2.length)
 {
     console.log(parola2);
-}else if(parola1<parola2)
+}else if(parola1.length<parola2.length)
 {
     console.log(parola1);
 }
